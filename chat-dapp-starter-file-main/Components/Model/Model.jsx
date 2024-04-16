298f5d1ba28b4a6547b4e0e71cff0b109def06d5
Kristine Thomas <kristinethomas@kristines-mbp.lan>
2024-04-15 22:53:27 -0500

    updates to UI

--- chat-dapp-starter-file-main/Components/Model/Model.jsx
+++ chat-dapp-starter-file-main/Components/Model/Model.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Image from "next/image";
 
-const Model = () => {
-  return <div>Model</div>;
+import Style from "./Model.module.css";
+import images from "../../assets";
+import { ChatAppContext } from "../../Context/ChatAppContext";
+import { Loader } from "../../Components/index";
+
+const Model = ({openModel, title, head, info, subInfo, image, functionName, address}) => {
+  const [name, setName] = useState("");
+  const [acctAddress, setAcctAddress] = useState("");
+
+  const { loading } = useContext(ChatAppContext);
+
+  return (
+    <div className={Style.Model}>
+      <div className={Style.Model_box}>
+        <div className={Style.Model_box_left}>
+          <Image src={image} alt="buddy" width={700} height={700} />
+        </div>
+        <div className={Style.Model_box_right}>
+          <h1>
+            {title} <span>{head}</span>
+          </h1>
+          <p>{info}</p>
+          <small>{subInfo}</small>
+
+          {
+            loading == true ? (
+              <Loader />
+            ) : (
+              <div className={Style.Model_box_right_name}>
+            <div className={Style.Model_box_right_name_info}>
+              <Image src={images.username} alt="user" width={30} height={30} />
+              <input type='text' placeholder="Enter Your preferred name" onChange={(e)=>setName(e.target.value)}/>
+            </div>
+            <div className={Style.Model_box_right_name_info}>
+              <Image src={images.account} alt="user" width={30} height={30} />
+              <input type='text' placeholder={address || "Enter Your Account Address"} onChange={(e)=>setAcctAddress(e.target.value)}/>
+            </div>
+
+            <div className={Style.Model_box_right_name_btn}>
+              <button onClick={() => functionName({ name, acctAddress })}>
+                {""}
+                <Image src={images.send} alt="send" width={30} height={30} />
+                {""}
+                Submit
+              </button>
+              <button onClick={() => openModel(false)}>
+                {""}
+                <Image src={images.close} alt="send" width={30} height={30} />
+                {""}
+                Close
+              </button>
+            </div>
+          </div>
+            )
+          }
+
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Model;
--- chat-dapp-starter-file-main/pages/_app.js
+++ chat-dapp-starter-file-main/pages/_app.js
@@ -1,5 +1,15 @@
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { ChatAppProvider } from "../Context/ChatAppContext";
+import { NavBar } from "../Components/index";
+
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <ChatAppProvider>
+      <NavBar />
+      <Component {...pageProps} />
+    </ChatAppProvider>
+  </div>
+);
+
+export default MyApp;
