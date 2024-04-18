9e60a9b11693274569c688d3468da88d32f23d77
kristom <25617730+Kristom123@users.noreply.github.com>
2024-04-18 16:09:05 -0500

    fixed process.env

--- chat-dapp-starter-file-main/Context/constants.js
+++ chat-dapp-starter-file-main/Context/constants.js
@@ -1,6 +1,6 @@
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
 import chatAppJSON from './ChatApp.json';
 
-export const ChatAppAddress = "0xDFE6D19A91E8471cBBc286a372F4a621f89Fa556";
+export const ChatAppAddress = "0x106037143D4b86737f8B160C52b7A87df82e503d";
 
 export const ChatAppABI = chatAppJSON.abi;
